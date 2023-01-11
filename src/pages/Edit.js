23f437a318844426007ab5bea804574024c48338
Jieun Pivarnik <JPivarnik@jis-macbook-pro.lan>
2023-01-10 23:01:29 -0600

    file structure and setup

---
+++ src/pages/Edit.js
@@ -0,0 +1,5 @@
+function Edit(props) {
+    return <h1>This is edit page</h1>;
+  }
+  
+  export default Edit;
---
+++ src/pages/Show.js
@@ -0,0 +1,5 @@
+function Show(props) {
+    return <h1>This is show page</h1>;
+  }
+  
+  export default Show;
