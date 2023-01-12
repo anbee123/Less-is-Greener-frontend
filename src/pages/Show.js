9caa8ff6d036666f1d05cb16eb4a9c1037ab7cc5
An Bui <anhipchipchip@gmail.com>
2023-01-11 23:50:09 -0500

    added link to url

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,24 +1,30 @@
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom"
 
 
 function Show(props) {
 
-  const { id }= useParams()
+  const { id } = useParams()
   const topic = props.topics.find((topic) => topic._id === id)
-    return (
-      <div className= "topics">
-        <h1>{topic.topic}</h1>
-        <img src={topic.image} alt={topic.topic} />
-    <h2>{topic.titleOne}</h2>
-   <h2>{topic.urlOne}</h2>
-   <h2>{topic.titleTwo}</h2>
-   <h2>{topic.urlTwo}</h2> 
-   <h2>{topic.titleThree}</h2>
-   <h2>{topic.urlThree}</h2>
+  return (
+    <div className="topics">
+      <h1>{topic.topic}</h1>
+      <img src={topic.image} alt={topic.topic} />
+      <h2>{topic.titleOne}</h2>
+      <Link to={`/topics/${topic._id}`}>
+        <h2>{topic.urlOne}</h2> </Link>
+      <h2>{topic.titleTwo}</h2>
+      <Link to={`/topics/${topic._id}`}>
+        <h2>{topic.urlTwo}</h2>
+      </Link>
+      <h2>{topic.titleThree}</h2>
+      <Link to={`/topics/${topic._id}`}>
+        <h2>{topic.urlThree}</h2>
+      </Link>
 
-      </div>
-    )
-   
-  }
-  
-  export default Show;
+    </div>
+  )
+
+}
+
+export default Show;
