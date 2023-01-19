078c8c2b3e2a4beb339b7b2bf396f536d36e2494
An Bui <anhipchipchip@gmail.com>
2023-01-19 01:59:24 -0500

    added more sites

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -10,7 +10,7 @@ const Resources = () => {
             <iframe width="560" height="315" src="https://www.youtube.com/embed/D-0QStAzmBA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </div>
 
-        <h3> </h3>
+        <h3>Resource sites:</h3>
             <ul>
                         <li><a href="https://www.rainforest-alliance.org/">Rainforest Alliance</a></li>
                         <li><a href="https://www.worldwildlife.org/places/forests">WWF - Forests</a></li>
@@ -18,6 +18,10 @@ const Resources = () => {
                         <li><a href="https://www.oceanconservancy.org/">Ocean Conservancy</a></li>
                         <li><a href="https://www.panda.org/oceans">WWF - Oceans</a></li>
                         <li><a href="https://www.oceanhero.today/">Ocean Hero</a></li>
+                        <li><a href="https://www.imperial.ac.uk/stories/climate-action/">9 things you can do about climate change</a></li>
+                        <li><a href="https://greenplanet4kids.com/">Green Planet For Kids</a></li>
+                        <li><a href="https://www.forbes.com/sites/amyblaschka/2018/09/27/if-you-want-to-change-your-life-change-your-environment/?sh=2cff14d1734d">If You Want To Change Your Life, Change Your Environment</a></li>
+                        <li><a href="https://www.psychologytoday.com/us/blog/habits-not-hacks/201408/want-change-your-habits-change-your-environment">Want to Change Your Habits? Change Your Environment</a></li>
                     </ul>
                 </div>
             
