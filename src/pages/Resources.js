a1d69d9f5cad73d00ea53bb1a5de688d1f9c80d2
Jieun Pivarnik <JPivarnik@jis-macbook-pro.lan>
2023-01-20 18:35:26 -0600

    resource-style

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -10,9 +10,9 @@ const Resources = () => {
             Making changes to be more environmentally friendly doesn't need to be the life altering decision that the media tends to display.  Small actions like minimizing our use of paper towels, taking the bus to school, or even bringing your lunch in reusable plastics can make a huge difference in our school's environmental footprint. </p>
             */}
         </div>
-
-        <div id="resource-list">
-            <p>Read more about our environment</p>
+        <section id="resource-container">
+            <div className="resource-list">
+            <h5 className="title">Read More About our Environment</h5>
             <ul>
                 <li><a href="https://www.rainforest-alliance.org/">What do Rainforest Alliance do?</a></li>
                 <li><a href="https://www.worldwildlife.org/initiatives/forests">Why Forest Matters?</a></li>
@@ -22,6 +22,19 @@ const Resources = () => {
                 <li><a href="https://www.oceanhero.today/">Ocean Hero</a></li>
              </ul>
         </div>
+        <div className="resource-list">
+            <h5 className="title">Fun Resources!</h5>
+            <ul>
+                <li><a href="https://seas.umich.edu/assets/games/quiz-game/">Grow and Test Your Green Knowledge</a></li>
+                <li><a href="https://seas.umich.edu/assets/games/wheel-of-sustainability/">Wheel of Sustainablity</a></li>
+                <li><a href="https://games4sustainability.org/">Sustainability through serious games and social stimulation</a></li>
+                <li><a href="https://berecycled.org/game/">Super Sorter Game: Sort recyclables by materials! </a></li>
+                <li><a href="https://seas.umich.edu/assets/games/day-in-life/">Day In The Life </a></li>
+                <li><a href="https://plays.org/design-squad-fidgit-power/">Design-Squid-Fidgit: Renewable Energy Game</a></li>
+             </ul>
+        </div>
+        </section>
+        
             
         </div>
             
